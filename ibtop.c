3c5d5a5103ef0e3f997631b4ff21f57aa2d5603c
John L. Hammond <jhammond@ices.utexas.edu>
2011-11-17 17:18:59 -0600

    Works.

--- ibtop.c
+++ ibtop.c
@@ -19,13 +19,14 @@ int main(int argc, char *argv[])
   int sw_lid = 3229;
   int sw_port = 9;
 
-  int umad_fd = -1; /* umad_fd */
+  int umad_fd = -1;
   int umad_agent_id = -1;
+  int umad_timeout_ms = 15;
+  int umad_retries = 10;
 
-  int mad_timeout_ms = 15;
-  // int mad_retries = 10;
-
+#ifdef DEBUG
   umad_debug(9);
+#endif
 
   if (umad_init() < 0) {
     ERROR("cannot init libibumad: %m\n");
@@ -49,74 +50,32 @@ int main(int argc, char *argv[])
     goto out;
   }
 
-  /* XXX qkey 0x80010000 */
-
-#ifdef CALL_MAD_RPC
-  ib_rpc_t sw_pma_rpc = {
-    .mgtclass = IB_PERFORMANCE_CLASS,
-    .method = IB_MAD_METHOD_GET,
-    .attr = {
-      .id = IB_GSI_PORT_COUNTERS_EXT,
-      .mod = 0,
-    },
-    .timeout = mad_timeout,
-    .datasz = IB_PC_DATA_SZ,
-    .dataoffs = IB_PC_DATA_OFFS,
-  };
-
-  mad_set_field(sw_pma_buf, 0, IB_PC_PORT_SELECT_F, sw_port);
-  if (mad_rpc(mad_port, &sw_pma_rpc, &sw_id, sw_pma_buf, sw_pma_buf) == NULL) {
-    /* ... */
-  }
-#endif
-
-  uint64_t s_trid = 0x1010cafebabe8008;
+  uint64_t s_xid = 0x1010cafebabe8008;
 
   char s_buf[1024], r_buf[1024];
   memset(s_buf, 0, sizeof(s_buf));
   memset(r_buf, 0, sizeof(r_buf));
 
   struct ib_user_mad *su = (struct ib_user_mad *) s_buf;
+  umad_set_addr(su, sw_lid, 1, 0, IB_DEFAULT_QP1_QKEY);
+  su->agent_id = umad_agent_id;
+  su->timeout_ms = umad_timeout_ms;
+  su->retries = umad_retries;
 
   void *sm = umad_get_mad(su);
-  /* char *p = mad_encode(mad, &sw_pma_rpc, 0, sw_pma_buf); */
-
   mad_set_field(sm, 0, IB_MAD_METHOD_F, IB_MAD_METHOD_GET);
-  mad_set_field(sm, 0, IB_MAD_RESPONSE_F, 0);
+  /* mad_set_field(sm, 0, IB_MAD_RESPONSE_F, 0); */
+  /* mad_set_field(sm, 0, IB_MAD_STATUS_F, 0 ); *//* rpc->rstatus */
   mad_set_field(sm, 0, IB_MAD_CLASSVER_F, 1);
   mad_set_field(sm, 0, IB_MAD_MGMTCLASS_F, IB_PERFORMANCE_CLASS);
   mad_set_field(sm, 0, IB_MAD_BASEVER_F, 1);
-
-  // mad_set_field(sm, 0, IB_MAD_STATUS_F, 0 /* rpc->rstatus */);
-  mad_set_field64(sm, 0, IB_MAD_TRID_F, s_trid);
+  mad_set_field64(sm, 0, IB_MAD_TRID_F, s_xid);
   mad_set_field(sm, 0, IB_MAD_ATTRID_F, IB_GSI_PORT_COUNTERS_EXT);
-  mad_set_field(sm, 0, IB_MAD_ATTRMOD_F, 0 /* rpc->attr.mod */);
-  mad_set_field64(sm, 0, IB_MAD_MKEY_F, 0 /* rpc->mkey */);
+  /* mad_set_field(sm, 0, IB_MAD_ATTRMOD_F, 0); *//* rpc->attr.mod */
+  /* mad_set_field64(sm, 0, IB_MAD_MKEY_F, 0); *//* rpc->mkey */
 
   void *s_data = (char *) sm + IB_PC_DATA_OFFS;
   mad_set_field(s_data, 0, IB_PC_PORT_SELECT_F, sw_port);
-
-  /* memcpy(mad + IB_PC_DATA_OFFS, sw_pma_buf, IB_PC_DATA_SZ); */
-  /* p = mda + IB_MAD_SIZE. */
-  /* len = p - mad; */
-
-  // ns = _do_madrpc(mad_port->port_id, umad_snd_buf, umad_rcv_buf,
-  // mad_port->class_agents[IB_PERFORMANCE_CLASS],
-  // mad_len, mad_timeout, mad_retries);
-
-  /* BEGIN umad_send() */
-  // ((struct ib_user_mad *) umad)->timeout_ms = mad_timeout_ms;
-  // ((struct ib_user_mad *) umad)->retries = mad_retries;
-  // ((struct ib_user_mad *) umad)->agent_id = mad_agent_id;
-
-  umad_set_addr(su, sw_lid, 1, 0, IB_DEFAULT_QP1_QKEY);
-  su->agent_id = umad_agent_id;
-  su->timeout_ms = mad_timeout_ms;
-  // su->retries = mad_retries;
-  /* length? */
-  // su->addr.qpn = 1;
-  // su->addr.qkey = IB_DEFAULT_QP1_QKEY;
-  // su->addr.lid = sw_lid;
 
   ssize_t nw = write(umad_fd, su, umad_size() + IB_MAD_SIZE);
   if (nw < 0) { /* ... */
@@ -132,7 +91,7 @@ int main(int argc, char *argv[])
   /* TODO Fixup timeout. */
 
   while (1) {
-    int np = poll(&fds, 1, mad_timeout_ms);
+    int np = poll(&fds, 1, umad_timeout_ms);
     if (np < 0) {
       ERROR("error polling for mads: %m\n");
       goto out;
@@ -153,10 +112,11 @@ int main(int argc, char *argv[])
   }
 
   struct ib_user_mad *ru = (struct ib_user_mad *) r_buf;
-  TRACE("ru status %d\n", ru->status);
   void *rm = umad_get_mad(ru);
-  uint64_t r_trid = mad_get_field64(rm, 0, IB_MAD_TRID_F);
-  TRACE("s_trid %lx, r_trid %lx\n", (unsigned long) s_trid, (unsigned long) r_trid);
+  uint64_t r_xid = mad_get_field64(rm, 0, IB_MAD_TRID_F);
+
+  TRACE("ru status %d\n", ru->status);
+  TRACE("s_xid %lx, r_xid %lx\n", (unsigned long) s_xid, (unsigned long) r_xid);
 
   if (mad_get_field(rm, 0, IB_DRSMP_STATUS_F) == IB_MAD_STS_REDIRECT) {
     ERROR("received redirect\n");
@@ -164,7 +124,6 @@ int main(int argc, char *argv[])
   }
 
   void *r_data = (char *) rm + IB_PC_DATA_OFFS;
-
   uint64_t sw_rx_bytes, sw_rx_packets, sw_tx_bytes, sw_tx_packets;
   mad_decode_field(r_data, IB_PC_EXT_RCV_BYTES_F, &sw_rx_bytes);
   mad_decode_field(r_data, IB_PC_EXT_RCV_PKTS_F,  &sw_rx_packets);
